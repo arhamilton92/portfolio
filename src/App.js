c8419ae665886981f22459802b01feb46ae0e4ae
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-05 19:58:40 -0400

    added navbar and intro text.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Navbar from "./components/Nav";
+import "./assets/style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <h1>Helloooo</h1>
-            </div>
-          </div>
-        </div>
-      </header>
+    <div>
+    <div> 
+      <Navbar />
     </div>
+    <div className="container">
+      <div className="row parent mt-5">
+        <div className="col-12 w-100">
+          <h1 className="centered intro">Let's create something.</h1>
+        </div>
+      </div>
+    </div>
+  </div>
   );
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../assets/style.css";
+
+function Nav() {
+
+  return (
+      <div>
+    <nav className="navbar navbar-dark">
+    </nav>
+  <nav className="collapse navbar-dark pt-3 pb-3" id="navbarToggleExternalContent">
+      <div className="d-flex justify-content-center">
+          <a className="nav-item nav-link" ><h5>About Me</h5></a>
+          <a className="nav-item nav-link" ><h5>Portfolio</h5></a>
+          <a className="nav-item nav-link" ><h5>Contact</h5></a>
+      </div>
+  </nav>
+  <nav className="navbar navbar-light" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <a className="navbar-brand ml-5 mt-2" href="#"><h3>Andrea Hamilton</h3></a>
+      <div className="navbar-nav float-right text-right pr-3 ">
+      <button className="navbar-toggler mr-5" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+      </button>
+      </div>
+  </nav>
+  <nav className="navbar navbar-dark">
+    </nav>
+  </div>
+  );
+}
+
+export default Nav;
