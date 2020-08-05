1edf38fbbfc795fefb01ff28ef0a8011ee1ec747
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-05 17:29:19 -0400

    hello world! basic, near empty website.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>Helloooo</h1>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
