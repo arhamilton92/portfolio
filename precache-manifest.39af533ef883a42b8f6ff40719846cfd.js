6907692b5a15d7451f2bfa3d75a36df0c61bc51b
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-05 17:33:25 -0400

    Updates

---
+++ precache-manifest.39af533ef883a42b8f6ff40719846cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3dfd22756a721d0c4b95765f7fb24ea",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80925e9e5c00fd897d19",
+    "url": "/portfolio/static/js/main.96afc6a2.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
