558abcd11218515c5f43fda20cf7c4a49aceefbb
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-05 20:31:14 -0400

    added about me info. created basic cards for portfolio

--- src/App.js
+++ src/App.js
@@ -8,10 +8,31 @@ function App() {
     <div> 
       <Navbar />
     </div>
-    <div className="container">
-      <div className="row parent mt-5">
+    <div className="container-fluid parent">
+      <div className="row parent introrow mt-5">
         <div className="col-12 w-100">
           <h1 className="centered intro">Let's create something.</h1>
+          <p id="about"> My name's Andrea. 
+          <br></br> 
+          I'm a web designer, currently seeking creative challenges to test my skills.
+          <br></br>
+          <br></br>
+          Have a project in mind?
+          <span id="contact-me">&emsp; CONTACT ME</span> </p>
+        </div>
+      </div>
+      <div className="row cardrow"> 
+        <div className="col-2 card m-3">
+          hello
+        </div>
+        <div className="col-2 card m-3">
+          hello
+        </div>
+        <div className="col-2 card m-3">
+          hello
+        </div>
+        <div className="col-2 card m-3">
+          hello
         </div>
       </div>
     </div>
